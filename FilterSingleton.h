7f3b8bb212c0603fd1fd1744b0b901b9a6e83356
Mountainbrussells <brussell@Bens-MacBook-Pro.local>
2015-04-08 09:09:35 -0600

    add comment to singleton

--- FilterSingleton.h
+++ FilterSingleton.h
@@ -5,6 +5,8 @@
 //  Created by Ben Russell on 3/12/15.
 //  Copyright (c) 2015 Ben Russell. All rights reserved.
 //
+//  Using this singleton to operate as the filter property bank
+
 
 #import <Foundation/Foundation.h>
 
